3af12e3fd0411f7a1a671aeb68e44dabdbace28b
John Doe <kasri_r@yahoo.fr>
2020-08-29 19:45:16 +0200

    Blog de Jean Forteroche

--- view/add_article.php
+++ view/add_article.php
@@ -4,7 +4,7 @@
     <h1>Nouvel Article</h1>
     <?php include('form_article.php'); ?>
 
-    <a href="../public/index.php">Retour à l'accueil</a>
+    <!--<a href="../public/index.php">Retour à l'accueil</a>-->
 </div>
 
 </html>
