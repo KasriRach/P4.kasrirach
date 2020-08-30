06bf5fbe4284c594b3f35cf9c63ade826984903b
John Doe <kasri_r@yahoo.fr>
2020-08-30 18:23:12 +0200

    Blog Jean Forteroche

--- view/edit_article.php
+++ view/edit_article.php
@@ -1,7 +1,7 @@
 <?php $this->title = "Modifier article"; ?>
 
 <div>
-    <h1>Modifier Article</h1>
+    <h1>Modifier Chapitre</h1>
     <?php include('form_article.php'); ?>
     <a href="../public/index.php">Retour à l'accueil</a>
 </div>
